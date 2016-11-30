1b0073d000397028d621ec15a49fd86bcb4fcd24
Mateusz Muszak <mateusz.muszak.ext@nokia.com>
2016-11-30 18:26:44 +0100

    Refactored functions for buffer reading/writing

--- 10-bounded-queue/bounded_queue.h
+++ 10-bounded-queue/bounded_queue.h
@@ -4,6 +4,7 @@
 #include <array>
 #include <mutex>
 #include <thread>
+#include <condition_variable>
 
 template<typename T, std::size_t N>
 class bounded_queue
@@ -15,14 +16,15 @@ public:
 
         while(_size == N)
         {
-            lock.unlock();
-            std::this_thread::yield();
-            lock.lock();
+            _full_queue.wait(lock);
         }
+
+
 
         _buffer.at(_back) = std::move(t);
         _back = (_back + 1) % N;
         ++_size;
+        _empty_queue.notify_one();
     }
 
     T pop_front()
@@ -31,20 +33,21 @@ public:
 
         while(_size == 0)
         {
-            lock.unlock();
-            std::this_thread::yield();
-            lock.lock();
+            _empty_queue.wait(lock);
         }
 
         auto ret = _buffer.at(_front);
         _front = (_front + 1) % N;
         --_size;
+        _full_queue.notify_one();
 
         return ret;
     }
 
 private:
     std::mutex _mtx;
+    std::condition_variable _full_queue;
+    std::condition_variable _empty_queue;
 
     std::array<T,N> _buffer;
     std::size_t _front = 0;
