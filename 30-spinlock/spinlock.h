ed0d6ac8bec85b9adf6355aa147f491fe1862d93
Mateusz Muszak <mateusz.muszak.ext@nokia.com>
2016-12-01 14:25:39 +0100

    Added completed files for semaphore and spinlock implementations

--- 30-spinlock/spinlock.h
+++ 30-spinlock/spinlock.h
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <atomic>
+
 class spinlock
 {
 public:
-    void lock();
-    bool try_lock();
-    void unlock();
-};
+    void lock()
+    {
+        assert(!_locked);
+        _locked = true;
+    }
+    bool try_lock()
+    {
+        return !_locked.exchange(true);
+    }
+    void unlock()
+    {
+        assert(_locked);
+        _locked = false;
+    }
 
+private:
+    std::atomic<bool> _locked { false };
+};
